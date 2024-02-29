01b096c3d3feff2ef8efe4025542869e46a83217
TomTassin <ttassin150@gmail.com>
2024-02-29 21:00:31 +0100

    finition des classes

--- src/personnages/src/personnages/Gaulois.java
+++ src/personnages/src/personnages/Gaulois.java
@@ -3,8 +3,8 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
-	private int effetPotion=1;
-	
+	private int effetPotion = 1;
+
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
@@ -13,26 +13,27 @@ public class Gaulois {
 	public String getNom() {
 		return nom;
 	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public int getForce() {
-		return force;
-	}
-
-	public void setForce(int force) {
-		this.force = force;
-	}
-
-	public int getEffetPotion() {
-		return effetPotion;
-	}
-
-	public void setEffetPotion(int effetPotion) {
-		this.effetPotion = effetPotion;
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
 	}
 	
-}
+	 private String prendreParole() {
+		 return "Le gaulois " + nom + " : ";
+	}
+	 
+	 public void frapper(Romain romain) {
+		 System.out.println(nom + " envoie un grand coup dans la mâchoire de "
+		 + romain.getNom());
+		 romain.recevoirCoup(force / 3);
+	}
 
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
