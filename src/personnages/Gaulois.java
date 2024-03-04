2a4df3bfb7dba370406adeb9d8c7c66173c605e3
TomTassin <ttassin150@gmail.com>
2024-03-04 21:49:11 +0100

    TP1 ex4 fini

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -28,15 +28,21 @@ public class Gaulois {
 
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom());
-		romain.recevoirCoup(force / 3);
+		romain.recevoirCoup((force / 3)*effetPotion);
 	}
 
 	@Override
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
-
+	
+	public void boirePotion(int forcePotion) {
+		effetPotion=forcePotion;
+		System.out.println("Merci Druide, je sens que ma force est "+forcePotion+" fois décuplée");
+	}
+	
 	public static void main(String[] args) {
-
+		Gaulois gaulois = new Gaulois("gaulois",8);
+		gaulois.boirePotion(5);
 	}
 }
