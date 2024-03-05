45b0b3798eef3b7ab2ff87d41afb78c46a273b49
Tom Tassin <tst4584a@ensfsi.onmicrosoft.com>
2024-03-05 09:07:29 +0100

    TP2 assert fini

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -22,12 +22,15 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		assert force > 0;
+		int force1 = force;
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aie");
 		} else {
 			parler("J'abandonne...");
 		}
+		assert force1 > force;
 	}
 
 	public int getForce() {
@@ -36,7 +39,6 @@ public class Romain {
 
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
-		minus.prendreParole();
 		minus.parler("UN GAU... UN GAUGAU...");
 		minus.recevoirCoup(8);
 	}
