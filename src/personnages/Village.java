2c23f2cb5d629a42713b26a8af22ca04555e660d
Tom Tassin <tst4584a@ensfsi.onmicrosoft.com>
2024-03-05 08:50:46 +0100

    TP2 ex d) e) f) g) h) fini

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,6 +3,7 @@ package personnages;
 public class Village {
 	private String nom;
 	private Gaulois[] villageois;
+	private Chef chef;
 	private int nbVillageois = 0;
 
 	public Village(String nom, int nbVillageoisMaximum) {
@@ -20,7 +21,28 @@ public class Village {
 		nbVillageois = nbVillageois + 1;
 	}
 
-	public void trouverHabitant(int indiceVillageois) {
-
+	public Gaulois trouverHabitant(int indiceVillageois) {
+		return villageois[indiceVillageois];
 	}
+
+	public void afficherVillageois(Chef chef) {
+		System.out.println("Dans le village du chef " + chef.getNom() + " vivent les légendaires gaulois:");
+		for (int i = 0; i < nbVillageois; i++) {
+			System.out.println("- " + villageois[i].getNom());
+		}
+	}
+
+	public static void main(String[] args) {
+		Village village = new Village("Village des irréductibles", 30);
+		Chef abraracourcix = new Chef("Abraracourcix", 6, village);
+		Gaulois asterix = new Gaulois("Asterix", 8);
+		Gaulois obelix = new Gaulois("Obelix", 25);
+		village.ajouterHabitant(asterix);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois(abraracourcix);
+	}
+	// Gaulois gaulois = village.trouverHabitant(30);
+	/*
+	 * Gaulois gaulois = village.trouverHabitant(1); System.out.println(gaulois);
+	 */
 }
