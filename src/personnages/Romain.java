99e7b0939c4421f4e43454519f4f51e14be4350b
Tom Tassin <tst4584a@ensfsi.onmicrosoft.com>
2024-03-05 09:49:51 +0100

    TP2 Ex2 a) b) c) fini

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,6 +3,8 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipements = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -37,9 +39,30 @@ public class Romain {
 		return force;
 	}
 
+	private void sEquiper(Equipement equipement) {
+		switch (nbEquipements) {
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+		case 1:
+			if (equipements[0] == equipement) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
+			} else {
+				nbEquipements += 1;
+				equipements[1] = equipement;
+				System.out.println("Le soldat " + nom + " s’équipe avec un " + equipement + ".");
+			}
+			break;
+		default:
+			equipements[0] = equipement;
+			nbEquipements += 1;
+			System.out.println("Le soldat " + nom + " s’équipe avec un " + equipement + ".");
+		}
+
+	}
+
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
-		minus.parler("UN GAU... UN GAUGAU...");
-		minus.recevoirCoup(8);
+		// minus.sEquiper(Equipement.CASQUE);
 	}
 }
