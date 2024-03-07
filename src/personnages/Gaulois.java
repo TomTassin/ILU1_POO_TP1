3b229aadd4238cf15d1d2dbd77d3689306a4e226
TomTassin <ttassin150@gmail.com>
2024-03-07 01:13:35 +0100

    debut TP3

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,6 +4,8 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
+	private int nb_trophees;
+	private Equipement[] trophees = new Equipement[100];
 
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
@@ -22,14 +24,44 @@ public class Gaulois {
 		System.out.println(prendreParole() + "<< " + texte + ">>");
 	}
 
-	public String prendreParole() {
+//	public String prendreParole() {
+//		return "Le gaulois " + nom + " : ";
+//	}
+	
+	
+	
+	
+	private String prendreParole() {
 		return "Le gaulois " + nom + " : ";
 	}
+	
+	
+	
+	
 
+//	public void frapper(Romain romain) {
+//		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom());
+//		romain.recevoirCoup((force / 3)*effetPotion);
+//	}
+	
+	
+	
+	
+	
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom());
-		romain.recevoirCoup((force / 3)*effetPotion);
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		trophees = romain.recevoirCoup((force / 3) *effetPotion);
+		for (int i = 0; trophees != null && i < trophees.length; i++,this.nb_trophees++) {
+			this.trophees[this.nb_trophees] = trophees[i];
+		}
 	}
+	
+	
+	
+	
+	
+	
+	
 
 	@Override
 	public String toString() {
